7c9e4f84922d2919f315deaa71fbd3a0edd3c2ff
Your Name <you@example.com>
2024-11-08 02:18:45 +0100

    added an array data

---
+++ src/assets/Price.js
@@ -0,0 +1,22 @@
+// pricingPlans.js
+const price = [
+    {
+        name: 'Basic',
+        price: '100 kr/Mån',
+        features: ['Medlemsrabatter.', 'Tillgång till gruppträningsklasser.', 'Ett gratis PT-tillfälle'],
+      },
+      {
+        name: 'Standard',
+        price: '299 kr/Mån',
+        features: ['Rabatter på produkter', 'Tillgång till alla gym i din region', 'Online träning'],
+      },
+      {
+        name: 'Premium',
+        price: '499 kr/Mån',
+        features: ['Personlig träning och konsultationer.', 'Kampsport.', 'Kost- och näringsrådgivning'],
+      },
+    ];
+
+  
+  export default price;
+  
---
+++ src/assets/trainer.js
@@ -0,0 +1,41 @@
+import trainer1 from "../assets/trainer1.jpg"
+import trainer2 from "../assets/trainer2.jpg"
+import trainer3 from "../assets/trainer3.jpg"
+
+const trainer=[
+   {
+    id:1,
+    name:"john",
+    role:"Styrketränare",
+    image:trainer1,
+    socials: {
+        instagram: "https://www.instagram.com",
+        facebook: "https://www.facebook.com",
+        twitter: "https://www.twitter.com"
+      }
+   },
+   {
+    id:2,
+    name:"Jordan",
+    role:"Konditionstränare",
+    image:trainer2,
+    socials: {
+        instagram: "https://www.instagram.com",
+        facebook: "https://www.facebook.com",
+        twitter: "https://www.twitter.com"
+      }
+   },
+   {
+    id:3,
+    name:"Sara",
+    role:"Yoga Specialist",
+    image:trainer3,
+    socials: {
+        instagram: "https://www.instagram.com",
+        facebook: "https://www.facebook.com",
+        twitter: "https://www.twitter.com"
+      }
+   },
+];
+
+export default trainer;
